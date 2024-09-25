5bf92367e20cec628bf6e38e318079711584b558
Pacool84 <flopez_1384@hotmail.com>
2024-09-25 12:37:53 -0600

    [ADD] Ruta maestra o global, aplicando buenas practicas

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 //Archivo principal donde se configuraran todas las rutas
-
+const express = require('express');
 const productsRouter = require('./products.router'); //Importamos las rutas de products
 const usersRouter = require('./users.router'); //Importamos las rutas de users
 
 function routerApi(app) {
-  app.use('/products', productsRouter); //Definimos la ruta e importamos todo el modulo de rutas de products
-  app.use('/users', usersRouter); //Definimos la ruta e importamos todo el modulo de rutas de users
+  //Generando una ruta maestra o global
+  const router = express.Router();
+  app.use('/api/v1', router);
+
+  router.use('/products', productsRouter); //Definimos la ruta e importamos todo el modulo de rutas de products
+  router.use('/users', usersRouter); //Definimos la ruta e importamos todo el modulo de rutas de users
 }
 
 module.exports = routerApi;
